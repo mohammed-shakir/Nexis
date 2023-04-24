12c342875ad920ab280ec5c184192452e26b15b2
Mohammed Shakir <mohammedshakir010528@gmail.com>
2023-04-24 22:05:46 +0200

    Applied the theme

--- nexis/lib/themes/default_theme.dart
+++ nexis/lib/themes/default_theme.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
 final ThemeData appTheme = ThemeData(
-  // backgroundColor: const Color(0xFF800020),
   colorScheme: ColorScheme.fromSwatch().copyWith(
     primary: const Color(0xFF171c2a), // Usually used for background
     secondary: const Color(0xFF800020), // Usually used for buttons and stuff
@@ -9,15 +8,9 @@ final ThemeData appTheme = ThemeData(
   textTheme: const TextTheme(
     // bodyMedium
     displayMedium: TextStyle(
-    color: Colors.white,
-    fontFamily: 'Arial',
-    fontSize: 12,
-    ),
-    // button text
-    labelLarge: TextStyle(
-    color: Colors.white,
-    fontFamily: 'Arial',
-    fontSize: 16,
+      color: Colors.white,
+      fontFamily: 'Arial',
+      fontSize: 12,
     ),
   ),
 );
