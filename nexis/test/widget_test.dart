8df52090ff6239bf0c08d74f1aa63d364aea76b9
Mohammed Shakir <mohammedshakir010528@gmail.com>
2023-05-25 23:02:14 +0200

    Fixed some unit tests

--- nexis/test/widget_test.dart
+++ nexis/test/widget_test.dart
@@ -1,10 +1,23 @@
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:nexis/pages/main/home.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:nexis/main.dart';
 
 void main() {
-  testWidgets('Init test', (WidgetTester tester) async {
-    // Build our app and trigger a frame
-    await tester.pumpWidget(const MaterialApp(home: Home()));
+  group('Init test', () {
+    setUpAll(() async {
+      TestWidgetsFlutterBinding.ensureInitialized();
+      await Firebase.initializeApp();
+      FirebaseFirestore.instance.settings = const Settings(
+        host: 'localhost:8080', // Use the URL of the Firestore emulator
+        sslEnabled: false,
+        persistenceEnabled: false,
+      );
+    });
+
+    testWidgets('Renders entire app', (WidgetTester tester) async {
+      // Build our app and trigger a frame.
+      await tester.pumpWidget(const Nexis());
+    });
   });
 }
