e7397068169e3104ba9f13a443c42463988c13f7
mohammed-shakir <mohammedshakir010528rgmail.com>
2023-06-24 00:03:30 +0200

    added tooltip to servers

--- nexis/lib/widgets/server_button.dart
+++ nexis/lib/widgets/server_button.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
+import 'package:just_the_tooltip/just_the_tooltip.dart';
 
 class ServerButton extends StatefulWidget {
   final VoidCallback onPressed;
@@ -24,120 +26,108 @@ class ServerButton extends StatefulWidget {
 
 class ServerButtonState extends State<ServerButton> {
   bool isHovering = false;
+  late final JustTheController _tooltipController;
+  Timer? _hoverTimer;
+
+  @override
+  void initState() {
+    super.initState();
+    _tooltipController = JustTheController();
+  }
+
+  @override
+  void dispose() {
+    _tooltipController.dispose();
+    _hoverTimer?.cancel();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return MouseRegion(
-      onHover: (_) => setState(() => isHovering = true),
-      onExit: (_) => setState(() => isHovering = false),
-      child: Stack(
-        children: [
-          if (isHovering || widget.isSelected)
-            Positioned(
-              left: 0,
-              top: 0,
-              bottom: 0,
-              child: Center(
-                child: AnimatedContainer(
-                  duration: const Duration(milliseconds: 150),
-                  curve: Curves.easeInOut,
-                  width: 4,
-                  height: widget.isSelected ? 40 : 20,
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(10),
+      onHover: (_) {
+        if (!widget.isSelected) {
+          setState(() => isHovering = true);
+          _hoverTimer?.cancel();
+          _hoverTimer = Timer(const Duration(milliseconds: 200), () {
+            _tooltipController.showTooltip(immediately: true);
+          });
+        }
+      },
+      onExit: (_) {
+        _hoverTimer?.cancel();
+        setState(() {
+          isHovering = false;
+        });
+        _tooltipController.hideTooltip();
+      },
+      child: JustTheTooltip(
+        controller: _tooltipController,
+        content: Padding(
+          padding: const EdgeInsets.all(12),
+          child: Text(
+            widget.name,
+            style: const TextStyle(color: Colors.white, fontSize: 14),
+          ),
+        ),
+        backgroundColor: Colors.black,
+        preferredDirection: AxisDirection.right,
+        borderRadius: BorderRadius.circular(10),
+        child: Stack(
+          children: [
+            if (isHovering || widget.isSelected)
+              Positioned(
+                left: 0,
+                top: 0,
+                bottom: 0,
+                child: Center(
+                  child: AnimatedContainer(
+                    duration: const Duration(milliseconds: 150),
+                    curve: Curves.easeInOut,
+                    width: 4,
+                    height: widget.isSelected ? 40 : 20,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
                   ),
                 ),
               ),
-            ),
-          Center(
-            child: AnimatedContainer(
-              duration: const Duration(milliseconds: 150),
-              curve: Curves.easeInOut,
-              width: 50,
-              height: 50,
-              decoration: BoxDecoration(
-                color: widget.image == null
-                    ? (widget.isSelected || isHovering
-                        ? Theme.of(context).colorScheme.secondary
-                        : widget.backgroundColor)
-                    : widget.backgroundColor,
-                borderRadius: BorderRadius.circular(
-                    widget.isSelected || isHovering ? 15 : 50),
-                image: widget.image != null
-                    ? DecorationImage(
-                        image: widget.image!,
-                        fit: BoxFit.fill,
-                      )
-                    : null,
+            Center(
+              child: AnimatedContainer(
+                duration: const Duration(milliseconds: 150),
+                curve: Curves.easeInOut,
+                width: 50,
+                height: 50,
+                decoration: BoxDecoration(
+                  color: widget.image == null
+                      ? (widget.isSelected || isHovering
+                          ? Theme.of(context).colorScheme.secondary
+                          : widget.backgroundColor)
+                      : widget.backgroundColor,
+                  borderRadius: BorderRadius.circular(
+                      widget.isSelected || isHovering ? 15 : 50),
+                  image: widget.image != null
+                      ? DecorationImage(
+                          image: widget.image!,
+                          fit: BoxFit.fill,
+                        )
+                      : null,
+                ),
+                child: IconButton(
+                  onPressed: widget.onPressed,
+                  color: Colors.white,
+                  icon: widget.icon ??
+                      const Icon(
+                        Icons.photo,
+                        color: Colors.transparent,
+                      ),
+                ),
               ),
-              child: IconButton(
-                onPressed: widget.onPressed,
-                color: Colors.white,
-                icon: widget.icon ??
-                    const Icon(
-                      Icons.photo,
-                      color: Colors.transparent,
-                    ),
-              ),
             ),
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-
-/*
-// Tooltip
-
-@override
-  Widget build(BuildContext context) {
-    return Tooltip(
-      message: widget.name,
-      padding: const EdgeInsets.all(12),
-      verticalOffset: -20,
-      margin: const EdgeInsets.only(left: 70),
-      textStyle: const TextStyle(
-        color: Colors.white,
-        fontSize: 15,
-      ),
-      decoration: BoxDecoration(
-        color: Colors.black,
-        borderRadius: BorderRadius.circular(10),
-      ),
-      child: MouseRegion(
-        onHover: (_) => setState(() => isHovering = true),
-        onExit: (_) => setState(() => isHovering = false),
-        child: AnimatedContainer(
-          duration: const Duration(milliseconds: 150),
-          curve: Curves.easeInOut,
-          width: 50,
-          height: 50,
-          decoration: BoxDecoration(
-            color: widget.image == null
-                ? (widget.isSelected || isHovering
-                    ? Theme.of(context).colorScheme.secondary
-                    : widget.backgroundColor)
-                : widget.backgroundColor,
-            borderRadius: BorderRadius.circular(
-                widget.isSelected || isHovering ? 15 : 50),
-            image: widget.image != null
-                ? DecorationImage(
-                    image: widget.image!,
-                    fit: BoxFit.fill,
-                  )
-                : null,
-          ),
-          child: IconButton(
-            onPressed: widget.onPressed,
-            color: Colors.white,
-            icon: widget.icon ??
-                const Icon(Icons.photo, color: Colors.transparent),
-          ),
+          ],
         ),
       ),
     );
   }
-*/
+}
