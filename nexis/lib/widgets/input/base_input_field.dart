a352be2661ca8b768e1a9ae0a2752cca02bcf8c4
syko240 <andrepost@hotmail.se>
2023-11-11 00:28:05 +0100

    PasswordTextField and Update to BaseInputField

--- nexis/lib/widgets/input/base_input_field.dart
+++ nexis/lib/widgets/input/base_input_field.dart
@@ -1,3 +1,59 @@
 import 'package:flutter/material.dart';
 
-abstract class BaseInputField extends StatelessWidget {}
+abstract class BaseInputField extends StatefulWidget {
+  final Color? focusBorderColor;
+  const BaseInputField({
+    Key? key,
+    this.focusBorderColor,
+  }) : super(key: key);
+
+  InputDecoration fieldDecoration(BuildContext context) {
+    return InputDecoration(
+        hoverColor: Colors.transparent,
+        border: buildBorder(context),
+        focusedBorder: buildBorder(context, focusBorderColor),
+        filled: true,
+        fillColor: Colors.blueGrey[750],
+        hintStyle: Theme.of(context).textTheme.labelSmall,
+        contentPadding: const EdgeInsets.only(
+            right: 10.0, left: 10.0, top: 10, bottom: 10));
+  }
+
+  OutlineInputBorder buildBorder([context, color]) {
+    return OutlineInputBorder(
+      borderRadius: BorderRadius.circular(8),
+      borderSide: BorderSide(
+        color: color ?? Theme.of(context).colorScheme.outline,
+      ),
+    );
+  }
+
+  @override
+  State<BaseInputField> createState();
+}
+
+abstract class BaseInputFieldState<T extends BaseInputField> extends State<T> {
+  TextFormField textFormField({
+    bool obscureText = false,
+  }) {
+    return TextFormField(
+      style: Theme.of(context).textTheme.displayMedium,
+      cursorColor: Colors.white,
+      cursorHeight: 20.0,
+      cursorWidth: 1.2,
+      decoration: widget.fieldDecoration(context),
+      obscureText: obscureText,
+    );
+  }
+
+  Color setColor(bool hover) {
+    if (hover) {
+      return Colors.white;
+    } else {
+      return Colors.grey;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context);
+}
---
+++ nexis/lib/widgets/input/password_input_field.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:nexis/widgets/input/base_input_field.dart';
+
+class PasswordInputField extends BaseInputField {
+  const PasswordInputField({Key? key})
+      : super(key: key, focusBorderColor: const Color(0xFF800020));
+
+  @override
+  PasswordInputFieldState createState() => PasswordInputFieldState();
+}
+
+class PasswordInputFieldState extends BaseInputFieldState<PasswordInputField> {
+  late bool obscureTextIconHover = false;
+  bool obscureText = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(children: [
+      textFormField(obscureText: obscureText),
+      Positioned(
+          right: 0,
+          child: MouseRegion(
+            onEnter: (event) {
+              setState(() {
+                obscureTextIconHover = true;
+              });
+            },
+            onExit: (event) {
+              setState(() {
+                obscureTextIconHover = false;
+              });
+            },
+            child: IconButton(
+              padding: const EdgeInsets.fromLTRB(5.0, 8.5, 5.0, 0.0),
+              constraints: const BoxConstraints(maxWidth: 40.0),
+              iconSize: 30.0,
+              onPressed: () {
+                setState(() {
+                  obscureText = !obscureText;
+                });
+              },
+              hoverColor: Colors.transparent,
+              splashColor: Colors.transparent,
+              focusColor: Colors.transparent,
+              highlightColor: Colors.transparent,
+              icon: Icon(
+                obscureText ? Icons.visibility_off : Icons.visibility,
+                color: setColor(obscureTextIconHover),
+              ),
+            ),
+          )),
+    ]);
+  }
+}
